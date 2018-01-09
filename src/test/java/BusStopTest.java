8b68b1eb0e599198be7d2c6874e7f6d1c649a710
Robert Henderson <robert@nterpryz.com>
2018-01-09 15:05:34 +0000

    created busstop class

---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest {
+
+    BusStop busStop;
+
+    @Before
+    public void before(){
+        busStop = new BusStop("Jenners");
+    }
+
+}
